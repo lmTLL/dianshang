991b08a73e80f1cebc829199c10ea3862626b8a9
ImTLL <thefirstsecret@163.com>
2019-03-13 08:04:41 +0800

    第一张页面的端口
    解决模糊查询乱码问题

--- src/main/java/com/best/dianshang/DianshangApplication.java
+++ src/main/java/com/best/dianshang/DianshangApplication.java
@@ -3,10 +3,16 @@ package com.best.dianshang;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.best.dianshang.dao")
-public class DianshangApplication {
+public class DianshangApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DianshangApplication.class, args);
